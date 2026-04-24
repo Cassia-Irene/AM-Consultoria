369c0d8492ea2cfd3d2d815caebf8a156ac7d69f
IreneC16 <you@example.com>
2026-04-24 18:36:50 -0300

    feat: inicializar a estrutura do backend, páginas do frontend e dependências do ambiente virtual

---
+++ backend/src/services/faturamento_service.py
@@ -0,0 +1,1 @@
+# Cálculo valor_base + valor_extra
---
+++ frontend/src/mocks/clientes.ts
@@ -0,0 +1,98 @@
+export const clientesMock = [
+  {
+    id_cliente: 1,
+    nome_instituicao: "CAPS II Renascer",
+    tipo_instituicao: "CAPS",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 2,
+    nome_instituicao: "Lar São Francisco",
+    tipo_instituicao: "ILPI",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 3,
+    nome_instituicao: "UPA Cohab",
+    tipo_instituicao: "UPA",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 4,
+    nome_instituicao: "Hospital São Domingos",
+    tipo_instituicao: "Hospital",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 5,
+    nome_instituicao: "Pronto Socorro do Anil",
+    tipo_instituicao: "Pronto Socorro",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 6,
+    nome_instituicao: "CAPS III",
+    tipo_instituicao: "CAPS",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 7,
+    nome_instituicao: "SAMU",
+    tipo_instituicao: "SAMU",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 8,
+    nome_instituicao: "Unidade Básica de Saúde",
+    tipo_instituicao: "UBS",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "baixa"
+  },
+  {
+    id_cliente: 9,
+    nome_instituicao: "Policlínica",
+    tipo_instituicao: "Policlínica",
+    cidade: "São Luís",
+    status: "ativo",
+    nivel_complexidade: "baixa"
+  },
+  {
+    id_cliente: 10,
+    nome_instituicao: "Hospital Geral de Imperatriz",
+    tipo_instituicao: "Hospital",
+    cidade: "Imperatriz",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 11,
+    nome_instituicao: "Hospital Santa Cruz",
+    tipo_instituicao: "Hospital",
+    cidade: "Imperatriz",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  },
+  {
+    id_cliente: 12,
+    nome_instituicao: "Hospital São Camilo",
+    tipo_instituicao: "Hospital",
+    cidade: "Imperatriz",
+    status: "ativo",
+    nivel_complexidade: "alta"
+  }
+]

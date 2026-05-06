70adaf7732238777bf6ac50870937f8534c4919d
leonardo ferreira <leoferrza@gmail.com>
2026-05-06 00:23:23 -0300

    feat(cliente): ajustar modelo ClienteBase para incluir nome e adicionar validador para campos

--- backend/src/schemas/cliente.py
+++ backend/src/schemas/cliente.py
@@ -1,14 +1,22 @@
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
+from pydantic import BaseModel, field_validator # Importe o field_validator
+
 
 class ClienteBase(BaseModel):
-    nome_instituicao: str
+    nome: str
     tipo_instituicao: str
     cidade: str
     status: str = "ativo"
     nivel_complexidade: Optional[str] = None
     modalidade_atendimento: Optional[str] = None
     observacoes_gerais: Optional[str] = None
+
+    # Esse validador garante que 'Alta' vire 'alta' antes de chegar no banco
+    @field_validator('nivel_complexidade', 'status')
+    @classmethod
+    def para_minusculo(cls, v: str) -> str:
+        return v.lower()
 
 class ClienteCreate(ClienteBase):
     pass # Usado para o POST

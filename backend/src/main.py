8293e868c8e9c0fa7d7db45cce13ea3550784826
leonardo ferreira <leoferrza@gmail.com>
2026-05-08 15:36:41 -0300

    fix(main): remove recebimentos router from FastAPI application

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.routers import clientes, contatos, contratos, visitas, projetos, pendencias, eventos_criticos, visitas_extra, projeto_parcelas, projetos_extra, tipos_pagamento, contrato_pagamento, recebimentos, faturamento_cliente, entregas
+from src.routers import clientes, contatos, contratos, visitas, projetos, pendencias, eventos_criticos, visitas_extra, projeto_parcelas, projetos_extra, tipos_pagamento, contrato_pagamento, faturamento_cliente, entregas
 
 # 1º Criar a instância da aplicação
 app = FastAPI(title="AM Consultoria API")
@@ -17,7 +17,6 @@ app.include_router(projeto_parcelas.router)
 app.include_router(projetos_extra.router)
 app.include_router(tipos_pagamento.router)
 app.include_router(contrato_pagamento.router)
-app.include_router(recebimentos.router)
 app.include_router(faturamento_cliente.router)
 app.include_router(entregas.router)
 

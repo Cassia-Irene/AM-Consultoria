2fc645c4042e3840964f098d926a1672bbcf3ca7
leonardo ferreira <leoferrza@gmail.com>
2026-05-08 15:40:58 -0300

    fix(projeto_extra): adicionar campos de solicitante e aprovador ao modelo de ProjetoExtra

--- backend/src/models/projeto_extra.py
+++ backend/src/models/projeto_extra.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, Integer, Text, Numeric, ForeignKey
+from sqlalchemy import Boolean, Column, Date, Integer, Text, Numeric, ForeignKey
 from sqlalchemy.orm import relationship
 from src.database import Base
 
@@ -6,12 +6,10 @@ class ProjetoExtra(Base):
     __tablename__ = "projetos_extra"
 
     id_extra = Column(Integer, primary_key=True, index=True)
-    # 🔗 FK apontando para projetos
     id_projeto = Column(Integer, ForeignKey("projetos.id_projeto"), nullable=False)
-    
-    descricao_extra = Column(Text, nullable=False)
-    valor_extra = Column(Numeric(10, 2), nullable=False)
-    aprovado = Column(Boolean, nullable=False, default=False)
+    solicitado_por = Column(Integer, ForeignKey("contatos.id_contato"), nullable=False) # ID do usuário que solicitou o extra
+    aprovado_por = Column(Integer, ForeignKey("contatos.id_contato")) # ID do usuário que aprovou o extra (pode ser nulo se ainda não aprovado)
+
 
     # 🤝 Relacionamento
     projeto = relationship("Projeto", back_populates="extras")

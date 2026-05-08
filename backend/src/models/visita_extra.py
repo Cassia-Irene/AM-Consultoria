500457d043b9b13d0f8ee192f793d45c598ebf7b
leonardo ferreira <leoferrza@gmail.com>
2026-05-08 15:42:24 -0300

    fix(visita_extra): adicionar campo 'solicitado_por' e remover 'motivo_extra' no modelo VisitaExtra

--- backend/src/models/visita_extra.py
+++ backend/src/models/visita_extra.py
@@ -6,10 +6,8 @@ class VisitaExtra(Base):
     __tablename__ = "visitas_extra"
 
     id_extra = Column(Integer, primary_key=True, index=True)
-    # 🔗 FK apontando para visitas
     id_visita = Column(Integer, ForeignKey("visitas.id_visita"), nullable=False)
-    
-    motivo_extra = Column(Text, nullable=False)
+    solicitado_por = Column(Integer, ForeignKey("contatos.id_contato"), nullable=False) # ID do usuário que solicitou a visita extra
 
     # 🤝 Relacionamento
     visita = relationship("Visita", back_populates="visita_extra")

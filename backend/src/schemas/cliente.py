5024e2eb73a5f3360e69430ef677a7f64c7ded44
leonardo ferreira <leoferrza@gmail.com>
2026-05-08 21:21:22 -0300

    feat(cliente): adicionar classe ClienteUpdate para atualização de cliente

--- backend/src/schemas/cliente.py
+++ backend/src/schemas/cliente.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from pydantic import BaseModel, ConfigDict
 from typing import Optional
 from pydantic import BaseModel, field_validator # Importe o field_validator
@@ -26,3 +28,13 @@ class ClienteRead(ClienteBase):
     
     # Configuração necessária para o Pydantic entender o Model do SQLAlchemy
     model_config = ConfigDict(from_attributes=True)
+
+
+class ClienteUpdate(BaseModel):
+    nome: str | None = None
+    tipo_instituicao: str | None = None
+    cidade: str | None = None
+    status: str | None = None
+    nivel_complexidade: str | None = None
+    observacoes_gerais: str | None = None
+    

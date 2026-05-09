9fa7492f137861145fe5532d8cb15737e1ecf890
leonardo ferreira <leoferrza@gmail.com>
2026-05-08 21:21:57 -0300

    feat(tipo_pagamento): adicionar classe TipoPagamentoUpdate para atualização de tipo de pagamento

--- backend/src/schemas/tipo_pagamento.py
+++ backend/src/schemas/tipo_pagamento.py
@@ -10,3 +10,6 @@ class TipoPagamentoRead(TipoPagamentoBase):
     id_tipo: int
     
     model_config = ConfigDict(from_attributes=True)
+
+class TipoPagamentoUpdate(BaseModel):
+    tipo: str | None = None
